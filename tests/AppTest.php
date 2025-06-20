4023e2c591ba735e71d020cc442c33b0e3f575c9
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-06-20 17:09:00 +0530

    Update AppTest.php

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -2,11 +2,12 @@
 use PHPUnit\Framework\TestCase;
 use App\Calculator;
 
-require_once __DIR__ . '/../src/index.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-class AppTest extends TestCase {
-    public function testAddition() {
+class AppTest extends TestCase
+{
+    public function testAddition(): void
+    {
         $this->assertEquals(5, Calculator::add(2, 3));
     }
 }
-
